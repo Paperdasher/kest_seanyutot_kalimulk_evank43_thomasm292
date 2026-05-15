e8ac65a7232da2e6c7f68ba538ee3ddc62380d8e
Evan Khosh <evank43@nycstudents.net>
2026-05-15 08:50:51 -0400

    Added flask-side code for login and register pages from Mackey's old project

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@
 # 2026-06-01m
 
 from flask import Flask, render_template, request, flash, url_for, redirect, session
-
+import data
 
 app = Flask(__name__)
 
@@ -14,7 +14,60 @@ app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    if 'username' not in session:
+        return redirect(url_for("login"))
+
+    return render_template("home.html", username=session['username'])
+
+@app.route("/logout")
+def logout():
+    session.pop('username')
+    return redirect(url_for("login"))
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if 'username' in session:
+        return redirect(url_for("home"))
+
+    if request.method == 'POST':
+        username = request.form.get('username').strip().lower()
+        password = request.form.get('password').strip()
+
+        if not username or not password:
+            return render_template("register.html", error="Missing username or password")
+
+        if data.check_acc(username):
+            return render_template("register.html", error="Username already exists")
+
+        data.create_acc(username, password)
+
+        session['username'] = username
+        return redirect(url_for("home"))
+
+    return render_template("register.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if 'username' in session:
+        return redirect(url_for("home"))
+
+    if request.method == 'POST':
+        # store username and password as a variable
+        username = request.form.get('username').strip().lower()
+        password = request.form.get('password').strip()
+
+        if not username or not password:
+            return render_template('login.html', error="Missing username or password")
+
+        account = check_password(username)
+
+        if data.login_acc(username, password):
+            session["username"] = username
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", error="Username or password is incorrect")
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     # Run the Flask app in debug mode
